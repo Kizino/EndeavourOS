6b44652ee96fc69b3eb7e24d025dccae3cd7bbac
Thanh <thanhnguyen1206@gmail.com>
2025-04-01 22:56:07 -0500

    Update install.sh

--- post-install-script/install.sh
+++ post-install-script/install.sh
@@ -3,14 +3,14 @@
 # Update system
 sudo pacman -Syu --noconfirm
 
-# Install Flatpak
-sudo pacman -S --noconfirm flatpak
-
-# Add Flathub repository
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 # Install essential packages
 sudo pacman -S --noconfirm curl git vim
+
+# Install Flatpak
+# sudo pacman -S --noconfirm flatpak
+
+# Add Flathub repository
+# sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install Flatpak applications
 # flatpak install -y flathub com.visualstudio.code com.discordapp.Discord app.zen_browser.zen
