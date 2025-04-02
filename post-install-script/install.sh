84cb1123d617417a5bb66592d4277b1d7da4884f
Thanh <thanhnguyen1206@gmail.com>
2025-04-01 23:00:31 -0500

    Update install.sh

--- post-install-script/install.sh
+++ post-install-script/install.sh
@@ -6,14 +6,19 @@ sudo pacman -Syu --noconfirm
 # Install essential packages
 sudo pacman -S --noconfirm curl git vim
 
+# Install yay for AUR support
+sudo pacman -S --noconfirm yay
+
 # Install Flatpak
-# sudo pacman -S --noconfirm flatpak
+sudo pacman -S --noconfirm flatpak
 
 # Add Flathub repository
-# sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install Flatpak applications
 # flatpak install -y flathub com.visualstudio.code com.discordapp.Discord app.zen_browser.zen
 
+# Install AUR applications using yay (Uncomment if needed)
+# yay -S --noconfirm visual-studio-code-bin discord zenbrowser
+
 # Finished
-echo "Installation complete!"
